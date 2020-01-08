c258d38b9d21b2135907e78523c0b317140f97f0
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-08 18:22:11 +0800

    feat: express middleware
    - helmetjs
    - cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from "@nestjs/core";
+import helmet from "helmet";
+import cors from "cors";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.use(cors({
+    credentials: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
