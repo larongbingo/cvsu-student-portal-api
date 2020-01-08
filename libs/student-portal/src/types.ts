c03dfb9bfd98209585c717d8d58b9419006d8904
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-08 18:23:31 +0800

    feat: interfaces for the student portal module

---
+++ libs/student-portal/src/types.ts
@@ -0,0 +1,48 @@
+
+export interface ISubject {
+  subjectCode: string;
+  subjectTitle: string;
+  grade: string;
+  completion: string;
+  unit: string;
+  creditUnit: string;
+}
+
+export interface IProfile {
+  personalDetails: {
+    fullName: string;
+    birthDate: string;
+    sex: string;
+    religion: string;
+    civilStatus: string;
+  };
+
+  contactDetails: {
+    address: string;
+    guardian: string;
+    contactNumber: string;
+    emailAddress: string;
+  };
+
+  enrollmentDetails: {
+    course: string;
+    studentNumber: string;
+  };
+}
+
+export interface IEnrolledSubjects {
+  enrolledSubject: IEnrolledSubjects[];
+}
+
+export interface IEnrolledSubject {
+  scheduleCode: string;
+  subjectCode: string;
+  subjectTitle: string;
+  schedule: Array<{
+    time: string;
+    day: string;
+    room: string;
+  }>;
+  section: string;
+  instructor: string;
+}
