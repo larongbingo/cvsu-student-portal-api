4e4d5b94126b225fafb0373b7a9d31efc900ebaa
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-12 06:24:15 +0800

    chore: removed app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from "@nestjs/common";
 import { CvsuImusPortalModule } from "libs/cvsu-imus-portal/src";
 
 import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 
 @Module({
   imports: [CvsuImusPortalModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
