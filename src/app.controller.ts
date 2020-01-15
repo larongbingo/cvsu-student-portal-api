5aef4336a589f6053af7827e1647db8b0c982e78
Renz Christen Yeomer A. Pagulayan <larongbingo@gmail.com>
2020-01-15 18:38:36 +0800

    feat: add controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService } from "./app.service";
+import { Controller, Get, Query, Logger } from "@nestjs/common";
+import { ParsedStudentPortalService, ParsedPublicPortalService, StudentPortalService } from "cvsu-imus/student-portal";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly studentPortalService: StudentPortalService,
+    private readonly parsedStudentPortalService: ParsedStudentPortalService,
+    private readonly parsedPublicPortalService: ParsedPublicPortalService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/login")
+  public async GetSessionId(
+    @Query("studNum") studNum: string,
+    @Query("password") password: string,
+  ) {
+    const sessionId = await this.studentPortalService.LogIn(studNum, password);
+    return {sessionId};
+  }
+
+  @Get("/grades")
+  public async GetGrades(@Query("sessionId") sessionId: string) {
+    const grades = await this.parsedStudentPortalService.GetParsedGrades(sessionId);
+    return { grades };
+  }
+
+  @Get("/profile")
+  public async GetProfiles(@Query("sessionId") sessionId: string) {
+    const profile = await this.parsedStudentPortalService.GetParsedProfile(sessionId);
+    return { profile };
   }
 }
